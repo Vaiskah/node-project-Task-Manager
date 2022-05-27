5770b0510eb247cf8134603e4a6b38c6323df809
Vaiskah <kichuvaisakh@gmail.com>
2022-05-27 13:12:28 +0530

    Validation for post, Queries for getall task and get single task, method used creat, find, findOne

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,16 +1,35 @@
 const Task = require("../models/task");
 
-const getAllTasks = (req, res) => {
-  res.send("get all tasks");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const creatAllTasks = async (req, res) => {
-  const task = await Task.create(req.body); //Task.create({name:'first name', complete:'true'}) like this way
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body); //Task.create({name:'first name', complete:'true'}) like this way
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const getSingleTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getSingleTask = async (req, res) => {
+  try {
+    const {id:taskID} = req.params
+    const task = await Task.findOne({_id:taskID})
+    if(!task){
+      return res.status(404).json({msg:`No task with this id : ${taskID}`})
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+
 };
 
 const updateTask = (req, res) => {
--- models/task.js
+++ models/task.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  complete: Boolean,
+  name: {
+    type: String,
+    required: [true, "must provide name"],
+    trim: true,
+    maxlength: [20, "name cannot more than 20 characters"],
+  },
+  complete: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-module.exports = mongoose.model('Task', TaskSchema)
+module.exports = mongoose.model("Task", TaskSchema);
